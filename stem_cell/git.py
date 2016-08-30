6f6c7c5a10f6ce8788a68742f00f1d993aa1929e
Craig Bass <craig@madetech.com>
2016-08-30 17:37:53 +0100

    Fix integration defect with returned path

--- stem_cell/git.py
+++ stem_cell/git.py
@@ -3,8 +3,8 @@ import os
 
 class Git:
     def __init__(self, ):
-        self.path = '/tmp'
+        self.path = '/tmp/app-seed'
 
     def clone(self, url):
-        os.system("cd " + self.path + " ; git clone " + url)
+        os.system("git clone " + url + ' ' + self.path)
         return self.path
